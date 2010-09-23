fc6e9a46f646cb37abce51f86077152f103fd636
Ryan Heneise <ryan@mysmallidea.com>
2010-09-23 12:41:27 -0400

    Added models for API endpoints.

--- app/models/donation_type.rb
+++ app/models/donation_type.rb
@@ -1,5 +1,6 @@
-class DonationSplit < ActiveResource::Base
+class DonationType < ActiveResource::Base
   self.site = APP_CONFIG[:site]
   self.user = APP_CONFIG[:user]
   self.password = APP_CONFIG[:password]
+  self.prefix = "/settings/"
 end
---
+++ app/models/split.rb
@@ -0,0 +1,5 @@
+class Split < ActiveResource::Base
+  self.site = APP_CONFIG[:site]
+  self.user = APP_CONFIG[:user]
+  self.password = APP_CONFIG[:password]
+end
